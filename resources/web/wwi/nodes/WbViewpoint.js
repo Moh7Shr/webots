fe889c39b7b0a307ef65a372a4d3b545ebd3b782
ThomasOliverKimble <tokimble@gmail.com>
2022-03-14 11:51:51 +0100

    debugging

--- resources/web/wwi/nodes/WbViewpoint.js
+++ resources/web/wwi/nodes/WbViewpoint.js
@@ -216,6 +216,7 @@ export default class WbViewpoint extends WbBaseNode {
   }
 
   updatePosition() {
+    console.log("Going here");
     if (this.wrenObjectsCreatedCalled)
       this._applyPositionToWren();
 
